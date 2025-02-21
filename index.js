96b67977ed8da37c6949f2fdd8d37b85c6304b58
Luis Martín <xlmargim360@ieshnosmachado.org>
2025-02-21 10:08:31 +0100

    Primer commit
    Añadimos el proyecto

---
+++ index.js
@@ -0,0 +1,55 @@
+//importar libreria para manejo de configuracion
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV || "development"}`,
+});
+// Importar fichero de configuración con variables de entorno
+const config = require("./config/config");
+// Importar librería express --> web server
+const express = require("express");
+// Importar librería path, para manejar rutas de ficheros en el servidor
+const path = require("path");
+// Importar libreria CORS
+const cors = require("cors");
+// Importar gestores de rutas
+const publicacionRoutes = require("./routes/publicacionRoutes");
+const comentarioRoutes = require("./routes/comentarioRoutes");
+
+const app = express();
+// const port = process.env.PORT || 3000;
+
+// Configurar middleware para analizar JSON en las solicitudes
+app.use(express.json());
+
+// Configurar CORS para admitir solicitudes desde http://localhost:5173
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+// Configurar rutas de la API Rest
+app.use("/api/publicacion", publicacionRoutes);
+app.use("/api/comentario", comentarioRoutes);
+
+// Verificar que las rutas se están registrando
+console.log("Rutas activas:");
+console.log("GET /api/publicacion");
+console.log("GET /api/comentario");
+
+// Configurar el middleware para servir archivos estáticos desde el directorio 'public\old_js_vainilla'
+app.use(express.static(path.join(__dirname, "public")));
+
+// Ruta para manejar las solicitudes al archivo index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Iniciar el servidor solo si no estamos en modo de prueba
+if (process.env.NODE_ENV !== "test") {
+  app.listen(config.port, () => {
+    console.log(`Servidor escuchando en el puerto ${config.port}`);
+  });
+}
+// Exportamos la aplicación para poder hacer pruebas
+module.exports = app;
